12bf5b77011ddbf02712a9108c2a3f847fad9432
mowtiie <mowtiie.dev@gmail.com>
2026-07-30 21:09:25 +0800

    Added datagen for names and textures for items

--- src/main/java/com/mowtiie/unomod/UnoModDataGenerator.java
+++ src/main/java/com/mowtiie/unomod/UnoModDataGenerator.java
@@ -1,11 +1,14 @@
 package com.mowtiie.unomod;
 
+import com.mowtiie.unomod.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class UnoModDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		var pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModModelProvider::new);
 	}
 }
---
+++ src/main/java/com/mowtiie/unomod/datagen/ModModelProvider.java
@@ -0,0 +1,25 @@
+package com.mowtiie.unomod.datagen;
+
+import com.mowtiie.unomod.item.ModItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ModelTemplates;
+
+public class ModModelProvider extends FabricModelProvider {
+
+    public ModModelProvider(FabricPackOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators itemModelGenerators) {
+        itemModelGenerators.generateFlatItem(ModItems.COFFEE, ModelTemplates.FLAT_ITEM);
+    }
+}
